3e579ca6707bc3559a5c486cf3766f19089b4da0
Emmanuel Durand <emmanueldurand@gmail.com>
2013-12-19 17:14:56 -0500

    Going forward...

---
+++ include/world.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2013 Emmanuel Durand
+ *
+ * This file is part of Splash.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * blobserver is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with blobserver.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * @world.h
+ * The World class
+ */
+
+#ifndef WORLD_H
+#define WORLD_H
+
+class World {
+    public:
+        /**
+         * Constructor
+         */
+        World();
+
+        /**
+         * Destructor
+         */
+        ~World();
+};
+
+ #endif // WORLD_H
