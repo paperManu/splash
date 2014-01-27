67a8eb5065e5312e0c11aef979fb29b83f9feaec
Emmanuel Durand <emmanueldurand@gmail.com>
2014-01-27 10:29:03 -0500

    Small correction to Image_Shmdata

--- src/image_shmdata.cpp
+++ src/image_shmdata.cpp
@@ -201,6 +201,7 @@ void Image_Shmdata::onData(shmdata_any_reader_t* reader, void* shmbuf, void* dat
                 p[0] = context->_yCbCrLUT[yValue][uValue][vValue][0];
                 p[1] = context->_yCbCrLUT[yValue][uValue][vValue][1];
                 p[2] = context->_yCbCrLUT[yValue][uValue][vValue][2];
+                p[4] = 255;
             }
         }
         else
