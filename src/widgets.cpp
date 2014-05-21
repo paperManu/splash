3187e5b256d3132ebd3e8ea20ba9a48427e628aa
Emmanuel Durand <emmanueldurand@gmail.com>
2014-05-21 08:39:38 -0400

    The eye is now moved with the target when performing a right click in the global view

--- src/widgets.cpp
+++ src/widgets.cpp
@@ -543,7 +543,10 @@ bool GlvGlobalView::onEvent(Event::t e, GLV& g)
             float dy = g.mouse().dy();
             auto scene = _scene.lock();
             if (_camera != _guiCamera)
+            {
                 scene->sendMessage("sendAll", {_camera->getName(), "moveTarget", 0, 0, dy / 100.f});
+                scene->sendMessage("sendAll", {_camera->getName(), "moveEye", 0, 0, dy / 100.f});
+            }
             else
                 _camera->setAttribute("moveTarget", {0, 0, dy / 100.f});
             return false;
