16912e4eacc6b744f91bafeea0e0e6218ac601e0
Emmanuel Durand <emmanueldurand@gmail.com>
2017-01-11 12:03:10 -0500

    Added _time (in ms) as a built-in uniform for Filter

--- src/filter.cpp
+++ src/filter.cpp
@@ -206,6 +206,9 @@ void Filter::updateUniforms()
 {
     auto shader = _screen->getShader();
 
+    // Built-in uniforms
+    _filterUniforms["_time"] = Values({static_cast<int>(Timer::getTime() / 1000)});
+
     // Update generic uniforms
     for (auto& weakObject : _linkedObjects)
     {
