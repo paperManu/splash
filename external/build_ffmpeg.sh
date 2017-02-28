c174dde508f919b59e1a8b8da68c5c4e2657e87a
Emmanuel Durand <emmanueldurand@gmail.com>
2017-02-27 21:13:14 -0500

    Added a static_analysis build target, using cppcheck. Also added cppcheck to gitlab CI

--- external/build_ffmpeg.sh
+++ external/build_ffmpeg.sh
@@ -2,7 +2,15 @@
 
 EXTERNALS_DIR=$(pwd)
 
+if hash nproc; then
+    CPU_COUNT=$(nproc)
+elif hash gnproc; then
+    CPU_COUNT=$(gnproc)
+else
+    CPU_COUNT=1
+fi
+
 cd ffmpeg
 ./configure --enable-libsnappy --enable-gpl --disable-doc --disable-ffserver --disable-ffplay --disable-ffprobe --disable-ffmpeg --prefix="$EXTERNALS_DIR/third_parties"
-make -j$(nproc)
+make -j${CPU_COUNT}
 make install
---
+++ tests/build_cppcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+EXTERNALS_DIR=$(pwd)
+
+if hash nproc; then
+    CPU_COUNT=$(nproc)
+elif hash gnproc; then
+    CPU_COUNT=$(gnproc)
+else
+    CPU_COUNT=1
+fi
+
+if [ ! -d cppcheck ]; then
+    git clone https://github.com/danmar/cppcheck/ cppcheck
+fi
+
+cd cppcheck
+if [ ! -f cppcheck ]; then
+    make -j${CPU_COUNT} SRCDIR=build CFGDIR=cfg HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function"
+fi
