paperManu/splash

commit 3959d616fe63c0f77de5e81752a3dca3d8aea0e0

Fixed Link hanging at destruction of Scene

src/core/link.cpp
@@ -20,12 +20,17 @@ Link::Link(RootObject* root, const string& name)
     {
         _rootObject = root;
         _name = name;
-        _context = make_shared<zmq::context_t>(2);
+        _context = make_unique<zmq::context_t>(2);
 
-        _socketMessageOut = make_shared<zmq::socket_t>(*_context, ZMQ_PUB);
-        _socketMessageIn = make_shared<zmq::socket_t>(*_context, ZMQ_SUB);
-        _socketBufferOut = make_shared<zmq::socket_t>(*_context, ZMQ_PUB);
-        _socketBufferIn = make_shared<zmq::socket_t>(*_context, ZMQ_SUB);
+        _socketMessageOut = make_unique<zmq::socket_t>(*_context, ZMQ_PUB);
+        _socketMessageIn = make_unique<zmq::socket_t>(*_context, ZMQ_SUB);
+        _socketBufferOut = make_unique<zmq::socket_t>(*_context, ZMQ_PUB);
+        _socketBufferIn = make_unique<zmq::socket_t>(*_context, ZMQ_SUB);
+
+        // High water mark set to zero for the outputs
+        int hwm = 0;
+        _socketMessageOut->setsockopt(ZMQ_SNDHWM, &hwm, sizeof(hwm));
+        _socketBufferOut->setsockopt(ZMQ_SNDHWM, &hwm, sizeof(hwm));
     }
     catch (const zmq::error_t& e)
     {
@@ -38,6 +43,7 @@ Link::Link(RootObject* root, const string& name)
     if (!socketPrefix.empty())
         _basePath += socketPrefix + string("_");
 
+    _running = true;
     _bufferInThread = thread([&]() { handleInputBuffers(); });
     _messageInThread = thread([&]() { handleInputMessages(); });
 }
@@ -45,6 +51,10 @@ Link::Link(RootObject* root, const string& name)
 /*************/
 Link::~Link()
 {
+    _running = false;
+    _bufferInThread.join();
+    _messageInThread.join();
+
     int lingerValue = 0;
     try
     {
@@ -55,13 +65,6 @@ Link::~Link()
     {
         Log::get() << Log::ERROR << "Link::" << __FUNCTION__ << " - Error while closing socket: " << e.what() << Log::endl;
     }
-
-    _socketMessageOut.reset();
-    _socketBufferOut.reset();
-
-    _context.reset();
-    _bufferInThread.join();
-    _messageInThread.join();
 }
 
 /*************/
@@ -74,12 +77,7 @@ void Link::connectTo(const string& name)
 
     try
     {
-        // High water mark set to zero for the outputs
-        int hwm = 0;
-        _socketMessageOut->setsockopt(ZMQ_SNDHWM, &hwm, sizeof(hwm));
-        _socketBufferOut->setsockopt(ZMQ_SNDHWM, &hwm, sizeof(hwm));
-
-        // TODO: for now, all connections are through IPC.
+        // For now, all connections are through IPC.
         _socketMessageOut->connect((_basePath + "msg_" + name).c_str());
         _socketBufferOut->connect((_basePath + "buf_" + name).c_str());
     }
@@ -114,26 +112,26 @@ void Link::connectTo(const std::string& name, RootObject* peer)
 /*************/
 void Link::disconnectFrom(const std::string& name)
 {
-    auto targetPointerIt = _connectedTargetPointers.find(name);
-    if (targetPointerIt != _connectedTargetPointers.end())
-    {
-        _connectedTargetPointers.erase(targetPointerIt);
-    }
-
     auto targetIt = find(_connectedTargets.begin(), _connectedTargets.end(), name);
     if (targetIt != _connectedTargets.end())
     {
         try
         {
-            _connectedTargets.erase(targetIt);
             _socketMessageOut->disconnect((_basePath + "msg_" + name).c_str());
             _socketBufferOut->disconnect((_basePath + "buf_" + name).c_str());
+            _connectedTargets.erase(targetIt);
         }
         catch (const zmq::error_t& e)
         {
             if (errno != ETERM)
                 Log::get() << Log::WARNING << "Link::" << __FUNCTION__ << " - Exception while disconnecting from " << name << ": " << e.what() << Log::endl;
         }
+    }
+
+    auto targetPointerIt = _connectedTargetPointers.find(name);
+    if (targetPointerIt != _connectedTargetPointers.end())
+    {
+        _connectedTargetPointers.erase(targetPointerIt);
     }
 }
 
@@ -326,7 +324,7 @@ void Link::freeOlderBuffer(void* data, void* hint)
 
     if (index >= ctx->_otgBuffers.size())
     {
-        Log::get() << Log::WARNING << "Link::" << __FUNCTION__ << " - Buffer to free not found in currently sent buffers list" << Log::endl;
+        Log::get() << Log::DEBUGGING << "Link::" << __FUNCTION__ << " - Buffer to free not found in currently sent buffers list" << Log::endl;
         return;
     }
     ctx->_otgBuffers.erase(ctx->_otgBuffers.begin() + index);
@@ -382,9 +380,10 @@ void Link::handleInputMessages()
             return values;
         };
 
-        while (true)
+        while (_running)
         {
-            _socketMessageIn->recv(&msg); // name of the target
+            if (!_socketMessageIn->recv(&msg, ZMQ_DONTWAIT)) // name of the target
+                continue;
             string name((char*)msg.data());
             _socketMessageIn->recv(&msg); // target's attribute
             string attribute((char*)msg.data());
@@ -406,8 +405,6 @@ void Link::handleInputMessages()
         if (errno != ETERM)
             Log::get() << Log::WARNING << "Link::" << __FUNCTION__ << " - Exception: " << e.what() << Log::endl;
     }
-
-    _socketMessageIn.reset();
 }
 
 /*************/
@@ -422,11 +419,12 @@ void Link::handleInputBuffers()
         _socketBufferIn->bind((_basePath + "buf_" + _name).c_str());
         _socketBufferIn->setsockopt(ZMQ_SUBSCRIBE, NULL, 0); // We subscribe to all incoming messages
 
-        while (true)
+        while (_running)
         {
             zmq::message_t msg;
 
-            _socketBufferIn->recv(&msg);
+            if (!_socketBufferIn->recv(&msg, ZMQ_DONTWAIT))
+                continue;
             string name((char*)msg.data());
 
             _socketBufferIn->recv(&msg);
@@ -441,8 +439,6 @@ void Link::handleInputBuffers()
         if (errno != ETERM)
             Log::get() << Log::WARNING << "Link::" << __FUNCTION__ << " - Exception: " << e.what() << Log::endl;
     }
-
-    _socketBufferIn.reset();
 }
 
 } // end of namespace

src/core/link.h
@@ -123,20 +123,22 @@ class Link
     RootObject* _rootObject;
     std::string _basePath{""};
     std::string _name{""};
-    std::shared_ptr<zmq::context_t> _context;
-    Spinlock _msgSendMutex;
-    Spinlock _bufferSendMutex;
+
+    std::unique_ptr<zmq::context_t> _context;
+    std::unique_ptr<zmq::socket_t> _socketBufferIn;
+    std::unique_ptr<zmq::socket_t> _socketBufferOut;
+    std::unique_ptr<zmq::socket_t> _socketMessageIn;
+    std::unique_ptr<zmq::socket_t> _socketMessageOut;
 
     std::vector<std::string> _connectedTargets;
     std::map<std::string, RootObject*> _connectedTargetPointers;
 
     bool _connectedToInner{false};
     bool _connectedToOuter{false};
+    bool _running{false};
 
-    std::shared_ptr<zmq::socket_t> _socketBufferIn;
-    std::shared_ptr<zmq::socket_t> _socketBufferOut;
-    std::shared_ptr<zmq::socket_t> _socketMessageIn;
-    std::shared_ptr<zmq::socket_t> _socketMessageOut;
+    Spinlock _msgSendMutex;
+    Spinlock _bufferSendMutex;
 
     std::deque<std::shared_ptr<SerializedObject>> _otgBuffers;
     Spinlock _otgMutex;

src/core/root_object.cpp
@@ -567,7 +567,7 @@ Json::Value RootObject::getRootConfigurationAsJson(const string& rootName)
 /*************/
 Values RootObject::sendMessageWithAnswer(const string& name, const string& attribute, const Values& message, const unsigned long long timeout)
 {
-    if (_link == nullptr)
+    if (!_link)
         return {};
 
     lock_guard<mutex> lock(_answerMutex);

src/core/root_object.h
@@ -186,8 +186,8 @@ class RootObject : public BaseObject
     std::unordered_map<std::string, int> _treeCallbackIds{};
     std::unordered_map<std::string, CallbackHandle> _attributeCallbackHandles{};
 
-    std::unique_ptr<Factory> _factory; //!< Object factory
-    std::shared_ptr<Link> _link;       //!< Link object for communicatin between World and Scene
+    std::unique_ptr<Factory> _factory{}; //!< Object factory
+    std::unique_ptr<Link> _link{};       //!< Link object for communicatin between World and Scene
     std::string _linkSocketPrefix{""}; //!< Prefix to add to shared memory socket paths
 
     Values _lastAnswerReceived{}; //!< Holds the last answer received through the link

src/core/scene.cpp
@@ -109,8 +109,6 @@ Scene::~Scene()
 
     _mainWindow->releaseContext();
 
-    _link->disconnectFrom("world");
-
 #ifdef DEBUG
     Log::get() << Log::DEBUGGING << "Scene::~Scene - Destructor" << Log::endl;
 #endif
@@ -756,7 +754,7 @@ void Scene::init(const string& name)
     _textureUploadWindow = getNewSharedWindow();
 
     // Create the link and connect to the World
-    _link = make_shared<Link>(this, name);
+    _link = make_unique<Link>(this, name);
     _link->connectTo("world");
     sendMessageToWorld("sceneLaunched", {});
 }

src/core/world.cpp
@@ -680,7 +680,7 @@ void World::init()
 
         if (_linkSocketPrefix.empty())
             _linkSocketPrefix = to_string(static_cast<int>(getpid()));
-        _link = make_shared<Link>(this, _name);
+        _link = make_unique<Link>(this, _name);
 
         registerAttributes();
         initializeTree();

commit 41846be20df39d41c6584f73ddc01a39b481723c

Fixed performance regression from commit 3959d616

src/core/link.cpp
@@ -1,6 +1,8 @@
 #include "./core/link.h"
 
 #include <algorithm>
+#include <chrono>
+#include <thread>
 
 #include "./core/attribute.h"
 #include "./core/buffer_object.h"
@@ -383,7 +385,10 @@ void Link::handleInputMessages()
         while (_running)
         {
             if (!_socketMessageIn->recv(&msg, ZMQ_DONTWAIT)) // name of the target
+            {
+                std::this_thread::sleep_for(1ms);
                 continue;
+            }
             string name((char*)msg.data());
             _socketMessageIn->recv(&msg); // target's attribute
             string attribute((char*)msg.data());
@@ -424,7 +429,10 @@ void Link::handleInputBuffers()
             zmq::message_t msg;
 
             if (!_socketBufferIn->recv(&msg, ZMQ_DONTWAIT))
+            {
+                std::this_thread::sleep_for(1ms);
                 continue;
+            }
             string name((char*)msg.data());
 
             _socketBufferIn->recv(&msg);