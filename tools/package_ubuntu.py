7780842eca114dbc2f90a7085ab924865eab9f52
Emmanuel Durand <emmanueldurand@gmail.com>
2021-02-22 13:25:49 -0500

    Fixed packaging script not keeping updated changelog

--- tools/package_ubuntu.py
+++ tools/package_ubuntu.py
@@ -261,6 +261,7 @@ def update_changelog(package: str, version: List[int]) -> None:
 
     subprocess.call([get_git_config("core.editor", "vim"), new_file_path])
     os.rename(new_file_path, orig_file_path)
+    git_add([orig_file_path])
 
 
 @atexit.register  # Only clean on exit if the release was successful.
