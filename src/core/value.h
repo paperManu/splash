6b1b08a995bd7d87549fde490bc2ea0b03bed801
Emmanuel Durand <emmanueldurand@gmail.com>
2021-04-07 14:10:16 -0400

    Fixed building on arm32v7

--- src/core/value.h
+++ src/core/value.h
@@ -209,7 +209,9 @@ struct Value
             }
             else if constexpr (std::is_integral_v<T>)
             {
-                _data = 0l;
+                // Integer precision needs to be enforced, otherwise this
+                // might not compile on certain architectures.
+                _data = (int64_t)0;
                 _type = Type::integer;
                 return std::get<int64_t>(_data);
             }
