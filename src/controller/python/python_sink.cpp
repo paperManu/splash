173e524484c534edf1325bec69554785c59ce36f
Christian Frisson <christian.frisson@gmail.com>
2022-04-01 19:19:15 +0000

    Automate mpa-bionic-arm64-jetson and mpa-focal-amd64-nvidia package generation with gitlab CI

--- src/controller/python/python_sink.cpp
+++ src/controller/python/python_sink.cpp
@@ -669,7 +669,9 @@ PyTypeObject PythonSink::pythonSinkType = {
     0,                                                   /* tp_del */
     0,                                                   /* tp_version_tag */
     0,                                                   /* tp_finalize */
+    #if PY_MAJOR_VERSION > 3 || PY_MINOR_VERSION >= 8
     0                                                    /* tp_vectorcall */
+    #endif
 };
 // clang-format on
 
