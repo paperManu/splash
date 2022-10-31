a4ffca2e79f16f1edc86d18f65554c824a0f6904
Emmanuel Durand <emmanueldurand@gmail.com>
2022-10-31 15:45:01 -0400

    Post-release version bump

--- addons/blender/splash/__init__.py
+++ addons/blender/splash/__init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "Splash output",
     "author": "Emmanuel Durand",
-    "version": (0, 9, 28),
+    "version": (0, 9, 29),
     "blender": (2, 80, 0),
     "location": "3D View > Toolbox, File > Export",
     "description": "Utility tools to connect Blender to the Splash videomapper",
