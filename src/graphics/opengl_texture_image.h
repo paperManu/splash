61104373eb1eedde0c1551d033a6924b48d741fc
Tarek Yasser <tarekspeed7@gmail.com>
2023-09-22 05:05:40 +0300

    Split `OpenGLTexture_Image::update` to a bunch of smaller functions.

--- src/graphics/opengl_texture_image.h
+++ src/graphics/opengl_texture_image.h
@@ -18,7 +18,8 @@ namespace Splash
 	    OpenGLTexture_Image(OpenGLTexture_Image&&) = delete;
 	    OpenGLTexture_Image& operator=(OpenGLTexture_Image&&) = delete;
 
-	    virtual void initFromPixelFormat(int width, int height) {
+	    virtual void initFromPixelFormat(int width, int height) 
+	    {
 		// Can probably be boiled down to a map of:
 		// 	pixelFormat -> { numChannels, bitPerChannel, channelType, formatName, texInternalFormat, texFormat, texType }
 		// But that's probably asking for trouble, let's keep it simple.
@@ -114,7 +115,8 @@ namespace Splash
 
 	    }
 
-	    virtual void initOpenGLTexture(const GLvoid* data) {
+	    virtual void initOpenGLTexture(const GLvoid* data) 
+	    {
 		// Create and initialize the texture
 		if (glIsTexture(_glTex))
 		    glDeleteTextures(1, &_glTex);
@@ -176,12 +178,12 @@ namespace Splash
 		glGetTextureParameteriv(_glTex, pname, params);
 	    }
 
-
 	    virtual void update() final
 	    {
 		// If _img is nullptr, this texture is not set from an Image
 		if (_img.expired())
 		    return;
+
 		const auto img = _img.lock();
 
 		if (img->getTimestamp() == _spec.timestamp)
@@ -189,7 +191,7 @@ namespace Splash
 
 		if (_multisample > 1)
 		{
-		    Log::get() << Log::ERROR << "Texture_Image::" << __FUNCTION__ << " - Texture " << _name << " is multisampled, and can not be set from an image" << Log::endl;
+		    Log::get() << Log::ERROR << "OpenGLTexture_Image::" << __FUNCTION__ << " - Texture " << _name << " is multisampled, and can not be set from an image" << Log::endl;
 		    return;
 		}
 
@@ -197,17 +199,71 @@ namespace Splash
 		const auto imgLock = img->getReadLock();
 
 		auto spec = img->getSpec();
-		Values srgb, flip, flop;
-		img->getAttribute("srgb", srgb);
-		img->getAttribute("flip", flip);
-		img->getAttribute("flop", flop);
+		const bool isCompressed = updateCompressedSpec(spec);
+		const auto internalAndDataFormat = updateInternalAndDataFormat(isCompressed, spec, img);
 
-		// Store the image data size
+		if(!internalAndDataFormat) 
+		    return;
+
+		const auto [internalFormat, dataFormat] = internalAndDataFormat.value();
+
+		const GLenum glChannelOrder = getChannelOrder(spec);
 		const int imageDataSize = spec.rawSize();
-		GLenum glChannelOrder = getChannelOrder(spec);
+		// Update the textures if the format changed
+		if (spec != _spec || !spec.videoFrame)
+		{
+		    updateGLTextureParameters(isCompressed);
+		    reallocateAndInitGLTexture(isCompressed, internalFormat, spec, glChannelOrder, dataFormat,  img, imageDataSize);
+		    if(!swapPBOs(spec, imageDataSize, img))
+			return;
+		}
+		// Update the content of the texture, i.e the image
+		else
+		{
+		    updateTextureFromPBO(isCompressed, internalFormat, spec, glChannelOrder, dataFormat,  img, imageDataSize);
+		}
+
+		_spec.timestamp = spec.timestamp;
+
+		updateShaderUniforms(spec, img);
+
+		if (_filtering && !isCompressed)
+		    generateMipmap();
+	    }
+
+	    virtual bool updatePbos(int width, int height, int bytes) final
+	    {
+		glDeleteBuffers(2, _pbos);
+		auto flags = GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
+		auto imageDataSize = width * height * bytes;
+
+		glCreateBuffers(2, _pbos);
+		glNamedBufferStorage(_pbos[0], imageDataSize, 0, flags);
+		glNamedBufferStorage(_pbos[1], imageDataSize, 0, flags);
+
+		_pbosPixels[0] = (GLubyte*)glMapNamedBufferRange(_pbos[0], 0, imageDataSize, flags);
+		_pbosPixels[1] = (GLubyte*)glMapNamedBufferRange(_pbos[1], 0, imageDataSize, flags);
+
+		if (!_pbosPixels[0] || !_pbosPixels[1])
+		{
+		    Log::get() << Log::ERROR << "OpenGLTexture_Image::" << __FUNCTION__ << " - Unable to initialize upload PBOs" << Log::endl;
+		    return false;
+		}
+
+		return true;
+	    }
+
+	    virtual void generateMipmap() final {
+		glGenerateTextureMipmap(_glTex);
+	    }
+
+	private:
+	    bool updateCompressedSpec(ImageBufferSpec& spec) const 
+	    {
 
 		// If the texture is compressed, we need to modify a few values
 		bool isCompressed = false;
+
 		if (spec.format == "RGB_DXT1")
 		{
 		    isCompressed = true;
@@ -223,151 +279,186 @@ namespace Splash
 		{
 		    isCompressed = true;
 		}
+		
+		return isCompressed;
+	    }
 
-		// Get GL parameters
+	    std::optional<std::pair<GLenum, GLenum>> updateUncompressedInternalAndDataFormat(const ImageBufferSpec& spec, const Values& srgb) 
+	    {
 		GLenum internalFormat;
 		GLenum dataFormat = GL_UNSIGNED_BYTE;
+
+		if (spec.channels == 4 && spec.type == ImageBufferSpec::Type::UINT8)
+		{
+		    dataFormat = GL_UNSIGNED_INT_8_8_8_8_REV;
+		    if (srgb[0].as<bool>())
+			internalFormat = GL_SRGB8_ALPHA8;
+		    else
+			internalFormat = GL_RGBA;
+		}
+		else if (spec.channels == 3 && spec.type == ImageBufferSpec::Type::UINT8)
+		{
+		    dataFormat = GL_UNSIGNED_BYTE;
+		    if (srgb[0].as<bool>())
+			internalFormat = GL_SRGB8_ALPHA8;
+		    else
+			internalFormat = GL_RGBA8;
+		}
+		else if (spec.channels == 2 && spec.type == ImageBufferSpec::Type::UINT8)
+		{
+		    dataFormat = GL_UNSIGNED_BYTE;
+		    internalFormat = GL_RG8;
+		}
+		else if (spec.channels == 1 && spec.type == ImageBufferSpec::Type::UINT16)
+		{
+		    dataFormat = GL_UNSIGNED_SHORT;
+		    internalFormat = GL_R16;
+		}
+		else
+		{
+		    Log::get() << Log::WARNING << "OpenGLTexture_Image::" << __FUNCTION__ << " - Unknown uncompressed format" << Log::endl;
+		    return {};
+		}
+
+		return {{ internalFormat, dataFormat }};
+	    }
+
+	    std::optional<std::pair<GLenum, GLenum>> updateCompressedInternalAndDataFormat(const ImageBufferSpec& spec, const Values& srgb) const
+	    {
+		GLenum internalFormat;
+		GLenum dataFormat = GL_UNSIGNED_BYTE;
+
+		if (spec.format == "RGB_DXT1")
+		{
+		    if (srgb[0].as<bool>())
+			internalFormat = GL_COMPRESSED_SRGB_S3TC_DXT1_EXT;
+		    else
+			internalFormat = GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
+		}
+		else if (spec.format == "RGBA_DXT5")
+		{
+		    if (srgb[0].as<bool>())
+			internalFormat = GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT;
+		    else
+			internalFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+		}
+		else if (spec.format == "YCoCg_DXT5")
+		{
+		    internalFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+		}
+		else
+		{
+		    Log::get() << Log::WARNING << "OpenGLTexture_Image::" << __FUNCTION__ << " - Unknown compressed format" << Log::endl;
+		    return {};
+		}
+
+		return {{ internalFormat, dataFormat }};
+	    }
+
+	    std::optional<std::pair<GLenum, GLenum>> updateInternalAndDataFormat(bool isCompressed, const ImageBufferSpec& spec, std::shared_ptr<Image> img) 
+	    {
+		Values srgb;
+		img->getAttribute("srgb", srgb);
+
+		if (isCompressed)
+		    return updateCompressedInternalAndDataFormat(spec, srgb);
+		else
+		    return updateUncompressedInternalAndDataFormat(spec, srgb);
+	    }
+
+
+	    void updateGLTextureParameters(bool isCompressed) 
+	    {
+		// glTexStorage2D is immutable, so we have to delete the texture first
+		glDeleteTextures(1, &_glTex);
+		glCreateTextures(_textureType, 1, &_glTex);
+
+		glTextureParameteri(_glTex, GL_TEXTURE_WRAP_S, _glTextureWrap);
+		glTextureParameteri(_glTex, GL_TEXTURE_WRAP_T, _glTextureWrap);
+
+		if (_filtering)
+		{
+		    if (isCompressed)
+			glTextureParameteri(_glTex, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		    else
+			glTextureParameteri(_glTex, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		    glTextureParameteri(_glTex, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		}
+		else
+		{
+		    glTextureParameteri(_glTex, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		    glTextureParameteri(_glTex, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		}
+
+	    }
+
+	    void reallocateAndInitGLTexture(bool isCompressed, GLenum internalFormat, const ImageBufferSpec& spec, GLenum glChannelOrder, GLenum dataFormat, std::shared_ptr<Image> img, int imageDataSize) const
+	    {
+		// Create or update the texture parameters
 		if (!isCompressed)
 		{
-		    if (spec.channels == 4 && spec.type == ImageBufferSpec::Type::UINT8)
-		    {
-			dataFormat = GL_UNSIGNED_INT_8_8_8_8_REV;
-			if (srgb[0].as<bool>())
-			    internalFormat = GL_SRGB8_ALPHA8;
-			else
-			    internalFormat = GL_RGBA;
-		    }
-		    else if (spec.channels == 3 && spec.type == ImageBufferSpec::Type::UINT8)
-		    {
-			dataFormat = GL_UNSIGNED_BYTE;
-			if (srgb[0].as<bool>())
-			    internalFormat = GL_SRGB8_ALPHA8;
-			else
-			    internalFormat = GL_RGBA8;
-		    }
-		    else if (spec.channels == 2 && spec.type == ImageBufferSpec::Type::UINT8)
-		    {
-			dataFormat = GL_UNSIGNED_BYTE;
-			internalFormat = GL_RG8;
-		    }
-		    else if (spec.channels == 1 && spec.type == ImageBufferSpec::Type::UINT16)
-		    {
-			dataFormat = GL_UNSIGNED_SHORT;
-			internalFormat = GL_R16;
-		    }
-		    else
-		    {
-			Log::get() << Log::WARNING << "Texture_Image::" << __FUNCTION__ << " - Unknown uncompressed format" << Log::endl;
-			return;
-		    }
+#ifdef DEBUG
+		    Log::get() << Log::DEBUGGING << "OpenGLTexture_Image::" << __FUNCTION__ << " - Creating a new texture" << Log::endl;
+#endif
+		    glTextureStorage2D(_glTex, _texLevels, internalFormat, spec.width, spec.height);
+		    glTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, glChannelOrder, dataFormat, img->data());
 		}
 		else if (isCompressed)
 		{
-		    if (spec.format == "RGB_DXT1")
-		    {
-			if (srgb[0].as<bool>())
-			    internalFormat = GL_COMPRESSED_SRGB_S3TC_DXT1_EXT;
-			else
-			    internalFormat = GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
-		    }
-		    else if (spec.format == "RGBA_DXT5")
-		    {
-			if (srgb[0].as<bool>())
-			    internalFormat = GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT;
-			else
-			    internalFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
-		    }
-		    else if (spec.format == "YCoCg_DXT5")
-		    {
-			internalFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
-		    }
-		    else
-		    {
-			Log::get() << Log::WARNING << "Texture_Image::" << __FUNCTION__ << " - Unknown compressed format" << Log::endl;
-			return;
-		    }
-		}
+#ifdef DEBUG
+		    Log::get() << Log::DEBUGGING << "OpenGLTexture_Image::" << __FUNCTION__ << " - Creating a new compressed texture" << Log::endl;
+#endif
 
-		// Update the textures if the format changed
-		if (spec != _spec || !spec.videoFrame)
+		    glTextureStorage2D(_glTex, _texLevels, internalFormat, spec.width, spec.height);
+		    glCompressedTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, internalFormat, imageDataSize, img->data());
+		}
+	    }
+
+	    bool swapPBOs(const ImageBufferSpec& spec, int imageDataSize, std::shared_ptr<Image> img) 
+	    {
+		if (!updatePbos(spec.width, spec.height, spec.pixelBytes()))
+		    return false;
+
+		// Fill one of the PBOs right now
+		auto pixels = _pbosPixels[0];
+		if (pixels != nullptr)
 		{
-		    // glTexStorage2D is immutable, so we have to delete the texture first
-		    glDeleteTextures(1, &_glTex);
-		    glCreateTextures(_textureType, 1, &_glTex);
-
-		    glTextureParameteri(_glTex, GL_TEXTURE_WRAP_S, _glTextureWrap);
-		    glTextureParameteri(_glTex, GL_TEXTURE_WRAP_T, _glTextureWrap);
-
-		    if (_filtering)
-		    {
-			if (isCompressed)
-			    glTextureParameteri(_glTex, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			else
-			    glTextureParameteri(_glTex, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-			glTextureParameteri(_glTex, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		    }
-		    else
-		    {
-			glTextureParameteri(_glTex, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glTextureParameteri(_glTex, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		    }
-
-		    // Create or update the texture parameters
-		    if (!isCompressed)
-		    {
-#ifdef DEBUG
-			Log::get() << Log::DEBUGGING << "Texture_Image::" << __FUNCTION__ << " - Creating a new texture" << Log::endl;
-#endif
-			glTextureStorage2D(_glTex, _texLevels, internalFormat, spec.width, spec.height);
-			glTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, glChannelOrder, dataFormat, img->data());
-		    }
-		    else if (isCompressed)
-		    {
-#ifdef DEBUG
-			Log::get() << Log::DEBUGGING << "Texture_Image::" << __FUNCTION__ << " - Creating a new compressed texture" << Log::endl;
-#endif
-
-			glTextureStorage2D(_glTex, _texLevels, internalFormat, spec.width, spec.height);
-			glCompressedTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, internalFormat, imageDataSize, img->data());
-		    }
-
-		    if (!updatePbos(spec.width, spec.height, spec.pixelBytes()))
-			return;
-
-		    // Fill one of the PBOs right now
-		    auto pixels = _pbosPixels[0];
-		    if (pixels != nullptr)
-		    {
-			memcpy((void*)pixels, img->data(), imageDataSize);
-		    }
-
-		    // And copy it to the second PBO
-		    glCopyNamedBufferSubData(_pbos[0], _pbos[1], 0, 0, imageDataSize);
-		    _spec = spec;
+		    memcpy((void*)pixels, img->data(), imageDataSize);
 		}
-		// Update the content of the texture, i.e the image
+
+		// And copy it to the second PBO
+		glCopyNamedBufferSubData(_pbos[0], _pbos[1], 0, 0, imageDataSize);
+		_spec = spec;
+
+		return true;
+	    }
+
+	    void updateTextureFromPBO(bool isCompressed, GLenum internalFormat, const ImageBufferSpec& spec, GLenum glChannelOrder, GLenum dataFormat, std::shared_ptr<Image> img, int imageDataSize) 
+	    {
+		// Copy the pixels from the current PBO to the texture
+		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, _pbos[_pboUploadIndex]);
+		if (!isCompressed)
+		    glTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, glChannelOrder, dataFormat, 0);
 		else
-		{
-		    // Copy the pixels from the current PBO to the texture
-		    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, _pbos[_pboUploadIndex]);
-		    if (!isCompressed)
-			glTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, glChannelOrder, dataFormat, 0);
-		    else
-			glCompressedTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, internalFormat, imageDataSize, 0);
-		    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+		    glCompressedTextureSubImage2D(_glTex, 0, 0, 0, spec.width, spec.height, internalFormat, imageDataSize, 0);
+		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 
-		    _pboUploadIndex = (_pboUploadIndex + 1) % 2;
+		_pboUploadIndex = (_pboUploadIndex + 1) % 2;
 
-		    // Fill the next PBO with the image pixels
-		    auto pixels = _pbosPixels[_pboUploadIndex];
-		    if (pixels != nullptr)
-			memcpy(pixels, img->data(), imageDataSize);
-		}
+		// Fill the next PBO with the image pixels
+		auto pixels = _pbosPixels[_pboUploadIndex];
+		if (pixels != nullptr)
+		    memcpy(pixels, img->data(), imageDataSize);
+	    }
 
-		_spec.timestamp = spec.timestamp;
-
+	    void updateShaderUniforms(const ImageBufferSpec& spec, const std::shared_ptr<Image> img)
+	    {
 		// If needed, specify some uniforms for the shader which will use this texture
 		_shaderUniforms.clear();
+
+		Values flip, flop;
+		img->getAttribute("flip", flip);
+		img->getAttribute("flop", flop);
 
 		// Presentation parameters
 		_shaderUniforms["flip"] = flip;
@@ -386,37 +477,8 @@ namespace Splash
 		    _shaderUniforms["encoding"] = {ColorEncoding::YCoCg};
 		else
 		    _shaderUniforms["encoding"] = {ColorEncoding::RGB}; // Default case: RGB
-
-		if (_filtering && !isCompressed)
-		    generateMipmap();
 	    }
 
-	    virtual bool updatePbos(int width, int height, int bytes) final
-	    {
-		glDeleteBuffers(2, _pbos);
-
-		auto flags = GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
-		auto imageDataSize = width * height * bytes;
-
-		glCreateBuffers(2, _pbos);
-		glNamedBufferStorage(_pbos[0], imageDataSize, 0, flags);
-		glNamedBufferStorage(_pbos[1], imageDataSize, 0, flags);
-
-		_pbosPixels[0] = (GLubyte*)glMapNamedBufferRange(_pbos[0], 0, imageDataSize, flags);
-		_pbosPixels[1] = (GLubyte*)glMapNamedBufferRange(_pbos[1], 0, imageDataSize, flags);
-
-		if (!_pbosPixels[0] || !_pbosPixels[1])
-		{
-		    Log::get() << Log::ERROR << "Texture_Image::" << __FUNCTION__ << " - Unable to initialize upload PBOs" << Log::endl;
-		    return false;
-		}
-
-		return true;
-	    }
-
-	    virtual void generateMipmap() final {
-		glGenerateTextureMipmap(_glTex);
-	    }
 
 	private:
 	    GLubyte* _pbosPixels[2];
