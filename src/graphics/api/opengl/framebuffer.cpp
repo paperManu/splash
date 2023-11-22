dcdb86f4992a837f5467656ac5d37cc7d210967a
Emmanuel Durand <emmanueldurand@gmail.com>
2023-11-22 14:42:41 -0500

    Brought back named framebuffers in OpenGL fbo implementation

--- src/graphics/api/opengl/framebuffer.cpp
+++ src/graphics/api/opengl/framebuffer.cpp
@@ -11,15 +11,14 @@ namespace Splash::gfx::opengl
 /*************/
 Framebuffer::Framebuffer()
 {
-    glGenFramebuffers(1, &_fbo);
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _fbo);
+    glCreateFramebuffers(1, &_fbo);
 
     if (!_depthTexture)
     {
         _depthTexture = std::make_shared<Texture_Image>(nullptr, std::make_unique<gfx::opengl::Texture_ImageGfxImpl>());
         assert(_depthTexture != nullptr);
         _depthTexture->reset(_width, _height, "D", _multisample);
-        glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _depthTexture->getTexId(), 0);
+        glNamedFramebufferTexture(_fbo, GL_DEPTH_ATTACHMENT, _depthTexture->getTexId(), 0);
     }
 
     if (!_colorTexture)
@@ -29,12 +28,12 @@ Framebuffer::Framebuffer()
         _colorTexture->reset(_width, _height, "RGBA", _multisample);
         _colorTexture->setAttribute("clampToEdge", {true});
         _colorTexture->setAttribute("filtering", {false});
-        glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _colorTexture->getTexId(), 0);
+        glNamedFramebufferTexture(_fbo, GL_COLOR_ATTACHMENT0, _colorTexture->getTexId(), 0);
     }
 
     GLenum fboBuffers[1] = {GL_COLOR_ATTACHMENT0};
-    glDrawBuffers(1, fboBuffers);
-    const auto status = glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER);
+    glNamedFramebufferDrawBuffers(_fbo, 1, fboBuffers);
+    const auto status = glCheckNamedFramebufferStatus(_fbo, GL_DRAW_FRAMEBUFFER);
     if (status != GL_FRAMEBUFFER_COMPLETE)
     {
         Log::get() << Log::ERROR << "Framebuffer::" << __FUNCTION__ << " - Error while initializing render framebuffer object: " << status << Log::endl;
@@ -110,8 +109,8 @@ void Framebuffer::setRenderingParameters()
     else
         _colorTexture->reset(spec.width, spec.height, "RGBA", _multisample, _cubemap);
 
-    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _depthTexture->getTexId(), 0);
-    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _colorTexture->getTexId(), 0);
+    glNamedFramebufferTexture(_fbo, GL_DEPTH_ATTACHMENT, _depthTexture->getTexId(), 0);
+    glNamedFramebufferTexture(_fbo, GL_COLOR_ATTACHMENT0, _colorTexture->getTexId(), 0);
 }
 
 /*************/
@@ -123,14 +122,12 @@ void Framebuffer::setSize(int width, int height)
     _depthTexture->setResizable(true);
     _depthTexture->setAttribute("size", {width, height});
     _depthTexture->setResizable(_automaticResize);
+    glNamedFramebufferTexture(_fbo, GL_DEPTH_ATTACHMENT, _depthTexture->getTexId(), 0);
 
     _colorTexture->setResizable(true);
     _colorTexture->setAttribute("size", {width, height});
     _colorTexture->setResizable(_automaticResize);
-
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _fbo);
-    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _depthTexture->getTexId(), 0);
-    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _colorTexture->getTexId(), 0);
+    glNamedFramebufferTexture(_fbo, GL_COLOR_ATTACHMENT0, _colorTexture->getTexId(), 0);
 
     _width = width;
     _height = height;
