2389173520c703ecb4d6a466e8ce9809a981bd39
Emmanuel Durand <emmanueldurand@gmail.com>
2024-03-15 14:18:20 -0400

    Fixed navigating through calibration points with tab when no point is set

--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -1339,6 +1339,9 @@ void Camera::registerAttributes()
 
     addAttribute("selectNextCalibrationPoint",
         [&](const Values&) {
+            if (_calibrationPoints.empty())
+                return true;
+
             _selectedCalibrationPoint = (_selectedCalibrationPoint + 1) % _calibrationPoints.size();
             return true;
         },
@@ -1347,6 +1350,9 @@ void Camera::registerAttributes()
 
     addAttribute("selectPreviousCalibrationPoint",
         [&](const Values&) {
+            if (_calibrationPoints.empty())
+                return true;
+
             if (_selectedCalibrationPoint == 0)
                 _selectedCalibrationPoint = _calibrationPoints.size() - 1;
             else
