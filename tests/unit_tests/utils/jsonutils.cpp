b761d07908c4a902dab12075af5484b6f004a701
Emmanuel Durand <emmanueldurand@gmail.com>
2025-05-10 09:28:12 -0400

    Fix unit tests for Json loading

--- tests/unit_tests/utils/jsonutils.cpp
+++ tests/unit_tests/utils/jsonutils.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 #include <doctest.h>
 #include <json/json.h>
@@ -11,33 +12,33 @@ using namespace Splash;
 /*************/
 TEST_CASE("Testing World::checkAndUpgradeConfiguration for a single Scene")
 {
-    auto filePath = Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.0.0.json";
-    Json::Value configuration_0_0_0;
-    Utils::loadJsonFile(filePath, configuration_0_0_0);
-    Utils::checkAndUpgradeConfiguration(configuration_0_0_0);
+    const std::vector<std::string> files = {Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.0.0.json",
+        Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.7.15.json",
+        Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.7.21.json",
+        Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.8.17.json",
+        Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.10.0.json",
+        Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.11.0.json"};
 
-    filePath = Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.7.15.json";
-    Json::Value configuration_0_7_15;
-    Utils::loadJsonFile(filePath, configuration_0_7_15);
-    Utils::checkAndUpgradeConfiguration(configuration_0_7_15);
+    std::vector<Json::Value> configs;
+    for (const auto& file : files)
+    {
+        Json::Value configuration;
+        Utils::loadJsonFile(file, configuration);
+        Utils::checkAndUpgradeConfiguration(configuration);
+        configs.push_back(configuration);
+    }
 
-    filePath = Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.7.21.json";
-    Json::Value configuration_0_7_21;
-    Utils::loadJsonFile(filePath, configuration_0_7_21);
-    Utils::checkAndUpgradeConfiguration(configuration_0_7_21);
+    assert(!configs.empty());
+    const auto& firstConfig = configs[0];
+    for (uint32_t i = 1; i < configs.size(); ++i)
+        CHECK(firstConfig == configs[i]);
+}
 
-    filePath = Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.8.17.json";
-    Json::Value configuration_0_8_17;
-    Utils::loadJsonFile(filePath, configuration_0_8_17);
-    Utils::checkAndUpgradeConfiguration(configuration_0_8_17);
-
-    filePath = Utils::getCurrentWorkingDirectory() + "/data/sample_scene_0.10.0.json";
-    Json::Value configuration_0_10_0;
-    Utils::loadJsonFile(filePath, configuration_0_10_0);
-    Utils::checkAndUpgradeConfiguration(configuration_0_10_0);
-
-    CHECK(configuration_0_0_0 == configuration_0_7_15);
-    CHECK(configuration_0_0_0 == configuration_0_7_21);
-    CHECK(configuration_0_0_0 == configuration_0_8_17);
-    CHECK(configuration_0_0_0 == configuration_0_10_0);
+/*************/
+TEST_CASE("Testing World::checkAndUpgradeConfiguration for a scene exported from the Blender addon")
+{
+    const std::string file = Utils::getCurrentWorkingDirectory() + "/data/sample_blender_scene.json";
+    Json::Value configuration;
+    Utils::loadJsonFile(file, configuration);
+    CHECK(Utils::checkAndUpgradeConfiguration(configuration));
 }
